74639a06b8cd25bae0018f98e1b659e41451d451
Phuc Hau <phuchau.fit@gmail.com>
2025-10-06 20:39:25 +0700

    done

---
+++ public/script/script.js
@@ -0,0 +1,12 @@
+// Lấy từng nút theo ID và gán sự kiện click
+document.getElementById("Duong").addEventListener("click", function() {
+    window.location.href = "../../public/page/profile/Duong/index.html"; // đổi URL thành trang của Duong
+});
+
+document.getElementById("Hau").addEventListener("click", function() {
+    window.location.href = "../../public/page/profile/Hau/index.html"; // đổi URL thành trang của Hau
+});
+
+document.getElementById("Vinh").addEventListener("click", function() {
+    window.location.href = "../../public/page/profile/Vinh/index.html"; // đổi URL thành trang của Vinh
+});
